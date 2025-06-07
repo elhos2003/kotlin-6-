4309eb06d453073b31d843e674488704fb0e6fd8
elhos2003 <145202889+elhos2003@users.noreply.github.com>
2025-06-07 14:04:36 +0500

    Create NoteDao.kt

---
+++ com.example.notes/data/local /NoteDao.kt
@@ -0,0 +1,23 @@
+package com.example.notes.data.local
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface NoteDao {
+    @Query("SELECT * FROM notes")
+    fun getNotes(): Flow<List<Note>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertNote(note: Note)
+
+    @Delete
+    suspend fun deleteNote(note: Note)
+
+    @Query("SELECT * FROM notes WHERE uid = :uid")
+    suspend fun getNoteById(uid: String): Note?
+}
