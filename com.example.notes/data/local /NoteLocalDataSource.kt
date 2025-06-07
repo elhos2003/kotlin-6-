5dd4d061221829e1872febe6a534b58c35c58bf3
elhos2003 <145202889+elhos2003@users.noreply.github.com>
2025-06-07 14:32:37 +0500

    Update NoteLocalDataSource.kt

--- com.example.notes/data/local /NoteLocalDataSource.kt
+++ com.example.notes/data/local /NoteLocalDataSource.kt
@@ -1,18 +1,19 @@
-class NoteLocalDataSource {
-    private val notesCache = mutableListOf<Note>()
+package com.example.notes.data.local
 
-    private val _notesFlow = MutableStateFlow<List<Note>>(emptyList())
-    val notesFlow: StateFlow<List<Note>> = _notesFlow.asStateFlow()
+import com.example.notes.domain.model.Note
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
+
+class NoteLocalDataSource {
+    private val _notes = MutableStateFlow<List<Note>>(emptyList())
+    val notesFlow: Flow<List<Note>> = _notes
 
     suspend fun saveNote(note: Note) {
-        notesCache.add(note)
-        _notesFlow.emit(notesCache) // إ UI 
+        _notes.update { currentNotes -> currentNotes + note }
     }
 
     suspend fun deleteNote(uid: String) {
-        notesCache.removeAll { it.uid == uid }
-        _notesFlow.emit(notesCache)
+        _notes.update { currentNotes -> currentNotes.filter { it.uid != uid } }
     }
-
-    fun getNotes(): Flow<List<Note>> = notesFlow
 }
